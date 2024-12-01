575b737ca5322ee0bdf4a7a39453b5c9dc471eb9
ryuhei202 <ryuhei202@docomo.ne.jp>
2024-12-01 22:04:01 +0900

    Playerを描画

--- script.js
+++ script.js
@@ -4,9 +4,27 @@ window.addEventListener("load", function () {
   canvas.width = 1280;
   canvas.height = 720;
 
+  ctx.fillStyle = "white";
+  ctx.lineWidth = 3;
+  ctx.strokeStyle = "white";
+
   class Player {
     constructor(game) {
       this.game = game;
+      this.collisionX = this.game.width * 0.5;
+      this.collisionY = this.game.height * 0.5;
+      this.collisionRadius = 30;
+    }
+
+    draw(context) {
+      context.beginPath();
+      context.arc(this.collisionX, this.collisionY, this.collisionRadius, 0, Math.PI * 2);
+      context.save();
+      context.globalAlpha = 0.5;
+      context.fill();
+      context.stroke();
+      context.restore();
+
     }
   }
 
@@ -18,10 +36,15 @@ window.addEventListener("load", function () {
       this.height = this.canvas.height;
       this.player = new Player(this);
     }
+
+    render(context) {
+      this.player.draw(context);
+    }
   }
 
   const game = new Game(canvas);
-  console.log(game);
+  game.render(ctx);
+
 
   function animate() {
   }
